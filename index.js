e1055460bf8fda081dde1c4509bc6f3763883681
lrz80 <36395271+lrz80@users.noreply.github.com>
2025-03-25 13:26:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("🚀 Backend activo y funcionando.");
 });
